4145b7114d664ab6757bb16cd2e332aa256abd74
nithinlatha <latha@appinessworld.com>
2017-08-01 09:17:57 +0530

    Added Different components & Life cycle

---
+++ src/Latha.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Latha extends Component {
+    componentWillReceiveProps (){
+      console.log('Yes Latha updated');
+    }
+    render() {
+        return (
+          <div>This is Latha component Name : {this.props.name}</div>
+        );
+    }
+}
+
+export default Latha;
