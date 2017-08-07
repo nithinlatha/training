1754e0c3b0a54e493cf3d13018ae67bdfd71245d
green sand <tskishorekumar@gmail.com>
2017-08-07 16:28:57 +0530

    Task 2 : In Deep Cycle

--- src/App.js
+++ src/App.js
@@ -1,45 +1,69 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Timer,Phone} from './components/Timer.js';
-import Latha from './Latha.js';
-import Kishore from './Kishore.js';
-
-const elem =(
-  <p>Hi it's stateless </p>
-
-
-const element = (
-  <h1>
-    Hello Prasanth!
-  </h1>
-);
-
+import Sample from './components/sample.js';
+import update from 'react-addons-update';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      name : 'kishore',
-      id : 0,
-      friends : ["Prasanth","Latha"]
+     welcome:'Task 2 : Lifecycle with multi-components',
+     color: {
+       sampleColor : '#fff',
+       lathaColor: '',
+       madhuriColor: '',
+       prasanthColor: ''
+     }
     };
+    this.updateSample = this.updateSample.bind(this);
+    this.updateLatha = this.updateLatha.bind(this);
+    this.updateMadhuri = this.updateMadhuri.bind(this);
+    this.updatePrasanth = this.updatePrasanth.bind(this);
+  }
+  updateSample(event){
+    this.setState({
+      color:update(this.state.color, {
+        sampleColor:{$set: event.target.value}
+      })
+    });
+  }
+  updateLatha(event){
+    this.setState({
+      color:update(this.state.color, {
+        lathaColor:{$set: event.target.value}
+      })
+    });
+  }
+  updateMadhuri(event){
+    this.setState({
+      color:update(this.state.color, {
+        prasanthColor:{$set: event.target.value}
+      })
+    });
+  }
+  updatePrasanth(event){
+    this.setState({
+      color:update(this.state.color, {
+        sampleColor:{$set: event.target.value}
+      })
+    });
   }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to {this.state.id} {this.state.name} friends: {
-              this.state.friends.map(function(index){
-                return(<p>{index}</p>)
-              })
-            } Training</h2><Timer user={this.state} na="prasanth" name={this.state.name} style={{backgroundColor:"#cdcdcd"}} /> <Phone />,
+          <h2>{this.state.welcome}</h2>
+          <h3>Enter Hex Value for Component</h3>
+
+          Sample:<input   type="text" onChange={this.updateSample}    value={this.state.color.sampleColor} />
+          Latha:<input    type="text" onChange={this.updateLatha}     value={this.state.color.lathaColor} />
+          Madhuri:<input  type="text" onChange={this.updateMadhuri}   value={this.state.color.madhuriColor} />
+          Prasanth:<input type="text" onChange={this.updatePrasanth}  value={this.state.color.prasanthColor} />
+
+          <Sample color={this.state.color}/>
         </div>
-        <p className="App-intro">
-          {elem}
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ src/components/sample.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Sample extends Component {
+  render() {
+    return (
+      <p className="sample" style={{backgroundColor:this.props.color.sampleColor}}>Hey im Sample</p>
+    );
+  }
+}
