d805199153224912116306aedd18c55122a3a6ec
nithinlatha <latha@appinessworld.com>
2017-08-22 17:30:16 +0530

    practicing event handler

--- src/Eventhandling.js
+++ src/Eventhandling.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Eventchild from './components/eventchild.js';
+import update from 'react-addons-update';
 
 class Event extends Component {
  constructor(props){
    super(props);
    this.state = {
-     initial : 'hai'
+     initial : 'hai',
+     practise : {
+       yamuna : 'android-developer',
+       mayank : 'BED'
+     }
    };
    this.changeState = this.changeState.bind(this);
+   this.practicestate=this.practicestate.bind(this);
  }
  changeState(value){
    console.log(value);
@@ -15,11 +21,19 @@ class Event extends Component {
      {initial : value}
    );
  }
+ practicestate(value){
+   this.setState({
+     practise:update(this.state.practise, {
+         yamuna:{$set:value}
+     })
+   });
+ }
+
   render() {
     return (
       <div>
         <h1>Im parent</h1>
-        <Eventchild changeValue={this.changeState} value={this.state.initial} />
+        <Eventchild  changeState={this.changeValue} practicestate={this.changebtnvalue} value={this.state.initial} />   
       </div>
     );
   }
--- src/components/eventchild.js
+++ src/components/eventchild.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+
+
  class Eventchild extends Component {
   updateValue(event){
     this.props.changeValue(event.target.value);
   }
   ongraph(){
-    this.props.changeValue("it from button");
+    this.props.changeValue("it is from button");
   }
+  practicegraph(){
+    this.props.changebtnvalue("i am practice button");
+
+  }
+
   render() {
     return (
       <div>
       <p className="sample">{this.props.value}</p>
-        <input onChange={this.props.changeValue.bind(this)} type="text"  />
-        <button onClick={this.ongraph.bind(this)} type="button" > Click to update </button>
-        <input className="messageCheckbox" onChange={this.props.changeValue.bind(this)} type="checkbox" value="Kishore" name="mailId[]" />Kishore
-        <input className="messageCheckbox" onChange={this.props.changeValue.bind(this)} type="checkbox" value="Prasanth" name="mailId[]" />Prasanth
+        <input onChange={this.updateValue.bind(this)} type="text"  />
+        <Button bsStyle="success" bsSize="large" onClick={this.ongraph.bind(this)} type="button" > Click to update </Button>
+        <input className="messageCheckbox" onChange={this.updateValue.bind(this)} type="checkbox" value="1" name="mailId[]" />Kishore
+        <input className="messageCheckbox" onChange={this.updateValue.bind(this)} type="checkbox" value="2" name="mailId[]" />Prasanth
+            <Button bsStyle="success" bsSize="large" onClick={this.practicegraph.bind(this)} type="button" > Click to practice </Button>
+        /*  <div className="bullet-img">
+             <h1>Hi..</h1>
+             <div className="image-div">
+                 <Image src="../images/light.jpg" />
+             </div>
+         </div>*/
       </div>
     );
   }
