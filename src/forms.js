4a2e6376e55a2395a8739b4d25a71ecfd5ad7e0b
nithinlatha <latha@appinessworld.com>
2017-08-26 14:55:24 +0530

    assignment3 done

--- src/forms.js
+++ src/forms.js
@@ -3,6 +3,13 @@ import Child from './components/condition-child.js';
 import Childtwo from './components/children-two.js';
 import './App.css';
 
+class Yamuna extends React.Component {
+  render() {
+    return (              <input type="email" className="form-control" id="email" />
+);
+  }
+}
+
 class Forms extends Component {
   constructor(props){
     super(props);
@@ -12,8 +19,9 @@ class Forms extends Component {
      email:"",
      data:{
        hasData:true,
-       name:"kishore"
-     }
+       name:"kishore",
+     },
+      childVisible: false,
     };
   }
   validate(){
@@ -32,39 +40,44 @@ class Forms extends Component {
       email:event.target.value
     })
   }
+  onClick() {
+    this.setState({childVisible: !this.state.childVisible});
+  }
+
   render() {
     if(!this.state.validation){
       return(null);
     }
     return (
-      <div className="App">
-      {
-        this.state.validation ?
-        <p>email is mandatory</p>
-        : null
-      }
-      <form>
-        <div className="form-group">
-          <label htmlFor="email">Email address:</label>
-          <input onChange={this.change.bind(this)} type="email" className="form-control" id="email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="pwd">Password:</label>
-          <input type="password" className="form-control" id="pwd" />
-        </div>
-        <div className="checkbox">
-          <label><input type="checkbox"/> Remember me</label>
-        </div>
-        <button onClick={this.validate.bind(this)} type="button" className="btn btn-default">Submit</button>
-        {
-          this.state.data.hasData ?
-          <Child  data={this.state.data}/>
-          :
-          <Childtwo />
-        }
-      </form>
+<div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email address:</label>
+            <input onChange={this.change.bind(this)} type="email" className="form-control" id="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="pwd">Password:</label>
+            <input type="password" className="form-control" id="pwd" />
+          </div>
+          <div className="checkbox">
+            <label><input type="checkbox"/> Remember me</label>
+          </div>
+          <button onClick={this.validate.bind(this)} type="button" className="btn btn-default">Submit</button>
+          {
+            this.state.data.hasData ?
+            <Child  data={this.state.data}/>
+            :
+            <Childtwo />
+          }
+        </form>
+            <button onClick={() => this.onClick()} type="button" className="btn btn-default">Submit</button>
+              {
+           this.state.childVisible
+             ? <Yamuna />
+             : null
+         }
+</div>
 
-      </div>
     );
   }
 
